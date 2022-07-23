6ba5f866805001d8e3c6b0b60d810dabdc5683a3
nnn <maxkambekxolbekov515@gmail.com>
2022-07-22 20:08:19 -0700

    05

--- bin/basic_func05.dart
+++ bin/basic_func05.dart
@@ -1,16 +1,14 @@
 // Create a function called func.
 
 // Return the value double type.
-int func(){
+double func(){
   return 4;
 }
 
 
 
 void main() {
-  int x;
-  x = 4;
-  print(x);
-  print(x);
+
+  print(func());
 
 }
