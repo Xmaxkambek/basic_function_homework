077ce8b44b50053a5e225c4a9eb0d39b86c0b2c6
nnn <maxkambekxolbekov515@gmail.com>
2022-07-22 22:00:08 -0700

    08

--- bin/basic_func08.dart
+++ bin/basic_func08.dart
@@ -1,6 +1,14 @@
-//  Create a function called func.
+//   Create a function called func.
 
 //  Create function arguments a.
 
-//  Increase the value of a to one and return.
-void main() {}
+//  Increase the value of a to one and return. 
+int func(int a){
+  return a+1;
+}
+
+
+
+void main() {
+  print(func(1));
+}
